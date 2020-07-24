3a60f4cef70ff4c9755348ef94bddf25c6a1e7cc
Suraj Gurung <laxhu2008@gmail.com>
2020-07-24 15:41:08 -0500

    initial commit

---
+++ src/main/java/co/gurung/gurungSpringJavaBrains/GurungSpringJavaBrainsApplication.java
@@ -0,0 +1,13 @@
+package co.gurung.gurungSpringJavaBrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GurungSpringJavaBrainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GurungSpringJavaBrainsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/gurung/gurungSpringJavaBrains/GurungSpringJavaBrainsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.gurung.gurungSpringJavaBrains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GurungSpringJavaBrainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
